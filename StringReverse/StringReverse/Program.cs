852f4991216619d52b84a13333f7aa25b4dc8751
fpsadana <fpsaldana@gmail.com>
2013-09-03 16:47:52 -0500

    initial commit

---
+++ StringReverse/StringReverse/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "reverse me";
+
+            Console.WriteLine("Original: " + s);
+            
+
+            Console.WriteLine();
+            Console.WriteLine("Reversed: " + test(s));
+
+
+            Console.ReadLine();
+        }
+
+            
+
+        public static string test(string s)
+            {
+                string rev = "";
+            //print the last index of the string and work your way backwards
+                for (int i = 0; i < s.Length; i++)
+                {
+                    rev = rev + s[s.Length - (i + 1)];
+                }
+                return rev;
+            }
+            
+    
+ 
+
+        
+    }
+}
